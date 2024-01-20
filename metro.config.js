b6ba31620dde3c29c876aad6d2db104cbb161a40
leaftail1880 <110915645+leaftaul1880@users.noreply.github.com>
2024-01-20 14:12:57 +0300

    fix resolutions

--- metro.config.js
+++ metro.config.js
@@ -3,5 +3,6 @@ const { getSentryExpoConfig } = require('@sentry/react-native/metro')
 
 const config = getSentryExpoConfig(__dirname)
 
-module.exports = { ...config, resolver: { ...config.resolver, blockList: [config.resolver.blockList, /(\/amplify\/.*)$/], }, };
-
+module.exports = {
+	...config,
+}
