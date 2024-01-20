329e79f62ec8db1c734145491287bf7d565b9869
leaftail1880 <110915645+leaftaul1880@users.noreply.github.com>
2024-01-20 13:56:20 +0300

    fiiix

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,5 @@ const { getSentryExpoConfig } = require('@sentry/react-native/metro')
 
 const config = getSentryExpoConfig(__dirname)
 
-module.exports = config
+module.exports = { ...config, resolver: { ...config.resolver, blockList: [config.resolver.blockList, /(\/amplify\/.*)$/], }, };
+
