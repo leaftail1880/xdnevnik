810e62219ce5a3bbf97b2b8cb24506260bd7a76e
leaftail1880 <110915645+leaftaul1880@users.noreply.github.com>
2024-03-08 19:04:21 +0300

    bump version

--- app.config.js
+++ app.config.js
@@ -23,7 +23,7 @@ const Config = {
 	expo: {
 		name: IS_DEV ? 'XDnevnik Dev Client' : 'XDnevnik',
 		slug: 'xdnevnik',
-		version: '0.13.2',
+		version: '0.14.0',
 		owner: 'leaftail1880',
 		orientation: 'portrait',
 		icon: './assets/icon.png',
