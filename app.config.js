c5b6ea64663e59030fd89a9d5c27647987adc868
leaftail1880 <110915645+leaftaul1880@users.noreply.github.com>
2024-04-05 15:09:03 +0300

    bump: 0.15.2

--- app.config.js
+++ app.config.js
@@ -41,7 +41,7 @@ const Config = {
 	expo: {
 		name: IS_DEV ? 'XDnevnik Dev Client' : 'XDnevnik',
 		slug: 'xdnevnik',
-		version: '0.15.1',
+		version: '0.15.2',
 		owner: 'leaftail1880',
 		orientation: 'portrait',
 		icon: './assets/icon.png',
@@ -52,7 +52,6 @@ const Config = {
 			color: splashBackgroundLight,
 		},
 
-		
 		ios: {
 			bundleIdentifier: id,
 			splash: splash,
