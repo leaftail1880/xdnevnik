ee5dd5ad600f3a9e92ddc66b11bda90377a7ce1c
Leaftail <110915645+leaftail1880@users.noreply.github.com>
2025-01-23 12:29:46 +0300

    Update lesson.ts

--- src/services/notifications/lesson.ts
+++ src/services/notifications/lesson.ts
@@ -126,7 +126,7 @@ function getLessonPeriod(previous: Lesson, current: Lesson) {
 	}
 	return { date, period }
 }
-let registered = false
+let registered = true
 async function showNotification(
 	lesson: Lesson,
 	now: number,
@@ -170,7 +170,7 @@ async function showNotification(
 
 			// only alert when lesson notification
 			onlyAlertOnce: LessonNotifStore.currentLesson === lessonId,
-                        asForegroundService: true,
+                        //asForegroundService: true,
 			
 			progress:
 				state === LessonState.going
