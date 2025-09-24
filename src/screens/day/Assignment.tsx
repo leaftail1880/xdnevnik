ec1e36ca3dd2075814d0a483638340e22909151a
leaftail1880 <110915645+leaftaul1880@users.noreply.github.com>
2025-09-24 12:08:52 +0300

    fix

--- src/screens/day/Assignment.tsx
+++ src/screens/day/Assignment.tsx
@@ -20,190 +20,192 @@ import { DiaryLessonProps } from './screen'
 // TODO support adding attachment
 
 export default observer(function DiaryAssignment({
-	assignment,
-	navigateToLessonMarks,
+  assignment,
+  navigateToLessonMarks,
 }: {
-	assignment: Assignment
+  assignment: Assignment
 } & DiaryLessonProps) {
-	const [showHomework, setShowHomework] = useState(
-		// Do not show long homework by default
-		!(
-			XSettings.collapseLongAssignmentText &&
-			assignment.assignmentName.length > 40
-		),
-	)
+  const [showHomework, setShowHomework] = useState(
+    // Do not show long homework by default
+    !(
+      XSettings.collapseLongAssignmentText &&
+      assignment.assignmentName.length > 40
+    ),
+  )
 
-	const attachments = AttachmentsStore
+  const attachments = AttachmentsStore
 
-	const attachment =
-		assignment.attachmentsExists &&
-		attachments.result &&
-		attachments.result?.filter(e => e.assignmentId === assignment.assignmentId)
+  const attachment =
+    assignment.attachmentsExists &&
+    attachments.result &&
+    attachments.result?.filter(e => e.assignmentId === assignment.assignmentId)
 
-	const showAttachments = showHomework && assignment.attachmentsExists
-	return (
-		<>
-			<View
-				style={{
-					flexDirection: 'row',
-					justifyContent: 'space-between',
-					alignContent: 'stretch',
-					alignItems: 'center',
-					gap: Spacings.s1,
-					marginTop: Spacings.s1,
-				}}
-			>
-				<ScrollTextCopyable
-					style={{ alignSelf: 'center', flex: 3 }}
-					onPress={
-						XSettings.collapseLongAssignmentText
-							? () => setShowHomework(!showHomework)
-							: undefined
-					}
-				>
-					{showHomework
-						? `${assignment.assignmentTypeName}: ${assignment.assignmentName}`
-						: '...'}
-				</ScrollTextCopyable>
+  const showAttachments = showHomework && assignment.attachmentsExists
+  return (
+    <>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignContent: 'stretch',
+          alignItems: 'center',
+          gap: Spacings.s1,
+          marginTop: Spacings.s1,
+        }}
+      >
+        <ScrollTextCopyable
+          style={{ alignSelf: 'center', flex: 3 }}
+          onPress={
+            XSettings.collapseLongAssignmentText
+              ? () => setShowHomework(!showHomework)
+              : undefined
+          }
+        >
+          {showHomework
+            ? `${assignment.assignmentTypeName}: ${assignment.assignmentName}`
+            : '...'}
+        </ScrollTextCopyable>
 
-				<Mark
-					mark={assignment.result ?? 'Нет'}
-					duty={assignment.duty}
-					weight={assignment.weight}
-					maxWeight={assignment.weight}
-					minWeight={assignment.weight}
-					style={{ padding: Spacings.s1, paddingHorizontal: Spacings.s2 }}
-					onPress={navigateToLessonMarks}
-				/>
-			</View>
-			{showAttachments &&
-				(attachments.fallback ||
-					(attachment && (
-						<View style={{ width: '100%' }}>
-							{attachment.map(e => (
-								<AttachmentFile attachment={e} key={e.attachmentId} />
-							))}
-						</View>
-					)))}
-		</>
-	)
+        <Mark
+          mark={assignment.result ?? ' '}
+          duty={assignment.duty}
+          weight={assignment.weight}
+          maxWeight={assignment.weight}
+          minWeight={assignment.weight}
+          style={{ padding: 2, paddingHorizontal: 2 }}
+          textStyle={{ fontSize: 10 }}
+          subTextStyle={{ fontSize: 7 }}
+          onPress={navigateToLessonMarks}
+        />
+      </View>
+      {showAttachments &&
+        (attachments.fallback ||
+          (attachment && (
+            <View style={{ width: '100%' }}>
+              {attachment.map(e => (
+                <AttachmentFile attachment={e} key={e.attachmentId} />
+              ))}
+            </View>
+          )))}
+    </>
+  )
 })
 
 const AttachmentFile = observer(function AttachmentFile({
-	attachment,
+  attachment,
 }: {
-	attachment: Attachment
+  attachment: Attachment
 }) {
-	const fileUri = `${FileSystem.cacheDirectory}${attachment.attachmentId}-${attachment.fileName}`
-	const isImage = /\.(jpeg|png|jpg)$/.test(attachment.fileName)
+  const fileUri = `${FileSystem.cacheDirectory}${attachment.attachmentId}-${attachment.fileName}`
+  const isImage = /\.(jpeg|png|jpg)$/.test(attachment.fileName)
 
-	const [progress, setProgress] = useState<null | string>(null)
-	const [size, setSize] = useState<false | number>(false)
+  const [progress, setProgress] = useState<null | string>(null)
+  const [size, setSize] = useState<false | number>(false)
 
-	useEffect(() => {
-		checkSize(fileUri, setSize)
-	}, [fileUri])
+  useEffect(() => {
+    checkSize(fileUri, setSize)
+  }, [fileUri])
 
-	return (
-		<Chip
-			mode="flat"
-			icon={isImage ? 'camera' : 'file'}
-			style={styles.attachmentFile}
-			onPress={async () => {
-				if (!size) {
-					const result = await downloadAttachment(
-						attachment,
-						fileUri,
-						setProgress,
-					)
-					checkSize(result.uri, setSize)
-				} else {
-					Logger.debug('File already downloaded. Size is', size)
-				}
+  return (
+    <Chip
+      mode="flat"
+      icon={isImage ? 'camera' : 'file'}
+      style={styles.attachmentFile}
+      onPress={async () => {
+        if (!size) {
+          const result = await downloadAttachment(
+            attachment,
+            fileUri,
+            setProgress,
+          )
+          checkSize(result.uri, setSize)
+        } else {
+          Logger.debug('File already downloaded. Size is', size)
+        }
 
-				setProgress(null)
-				const share = () => shareAsync(fileUri, { dialogTitle: 'Файл' })
-				Logger.debug('Sharing', fileUri)
+        setProgress(null)
+        const share = () => shareAsync(fileUri, { dialogTitle: 'Файл' })
+        Logger.debug('Sharing', fileUri)
 
-				if (isImage) {
-					ModalAlert.show(
-						'Вложение',
-						<View style={styles.attachmentModal}>
-							<Text>{attachment.fileName}</Text>
-							<Image source={{ uri: fileUri, width: 200, height: 400 }}></Image>
-							<Chip mode="flat" onPress={share}>
-								Поделиться
-							</Chip>
-						</View>,
-					)
-				} else share()
-			}}
-		>
-			<View style={styles.preview}>
-				<Text>{attachment.fileName}</Text>
-				{size ? <Size t={size} /> : undefined}
-				<Text>{progress}</Text>
-			</View>
-		</Chip>
-	)
+        if (isImage) {
+          ModalAlert.show(
+            'Вложение',
+            <View style={styles.attachmentModal}>
+              <Text>{attachment.fileName}</Text>
+              <Image source={{ uri: fileUri, width: 200, height: 400 }}></Image>
+              <Chip mode="flat" onPress={share}>
+                Поделиться
+              </Chip>
+            </View>,
+          )
+        } else share()
+      }}
+    >
+      <View style={styles.preview}>
+        <Text>{attachment.fileName}</Text>
+        {size ? <Size t={size} /> : undefined}
+        <Text>{progress}</Text>
+      </View>
+    </Chip>
+  )
 })
 
 const styles = StyleSheet.create({
-	attachmentFile: {
-		margin: Spacings.s1,
-		padding: Spacings.s1,
-	},
-	attachmentModal: {
-		alignItems: 'center',
-		justifyContent: 'center',
-		gap: Spacings.s1,
-	},
-	preview: { flex: 1, gap: Spacings.s1, flexDirection: 'row' },
+  attachmentFile: {
+    margin: Spacings.s1,
+    padding: Spacings.s1,
+  },
+  attachmentModal: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: Spacings.s1,
+  },
+  preview: { flex: 1, gap: Spacings.s1, flexDirection: 'row' },
 })
 
 async function downloadAttachment(
-	attachment: Attachment,
-	fileUri: string,
-	setProgress: React.Dispatch<string | null>,
+  attachment: Attachment,
+  fileUri: string,
+  setProgress: React.Dispatch<string | null>,
 ) {
-	const httpUri = API.join(ROUTES.getAttachment, attachment.attachmentId + '')
-	Logger.debug('Saving', attachment.fileName, 'from the', httpUri)
-	const downloader = FileSystem.createDownloadResumable(
-		httpUri,
-		fileUri,
-		{
-			headers: {
-				Authorization: 'Bearer ' + API.session?.access_token,
-			},
-		},
-		p => {
-			setProgress(
-				'Скачивание: ' +
-					((p.totalBytesWritten / p.totalBytesExpectedToWrite) * 100).toFixed(
-						2,
-					) +
-					'%',
-			)
-		},
-	)
+  const httpUri = API.join(ROUTES.getAttachment, attachment.attachmentId + '')
+  Logger.debug('Saving', attachment.fileName, 'from the', httpUri)
+  const downloader = FileSystem.createDownloadResumable(
+    httpUri,
+    fileUri,
+    {
+      headers: {
+        Authorization: 'Bearer ' + API.session?.access_token,
+      },
+    },
+    p => {
+      setProgress(
+        'Скачивание: ' +
+        ((p.totalBytesWritten / p.totalBytesExpectedToWrite) * 100).toFixed(
+          2,
+        ) +
+        '%',
+      )
+    },
+  )
 
-	const result = await downloader.downloadAsync()
-	if (!result || result.status !== 200) {
-		ModalAlert.show(
-			'Не удалось сохранить вложение',
-			'С аккаунта родителя можно скачать только некоторые вложения',
-			true,
-		)
-		throw new Error('Failed to download file')
-	}
+  const result = await downloader.downloadAsync()
+  if (!result || result.status !== 200) {
+    ModalAlert.show(
+      'Не удалось сохранить вложение',
+      'С аккаунта родителя можно скачать только некоторые вложения',
+      true,
+    )
+    throw new Error('Failed to download file')
+  }
 
-	return result
+  return result
 }
 
 function checkSize(fileUri: string, setSize: React.Dispatch<number | false>) {
-	FileSystem.getInfoAsync(fileUri).then(info => {
-		if (info.exists && !info.isDirectory) {
-			setSize(info.size)
-		}
-	})
+  FileSystem.getInfoAsync(fileUri).then(info => {
+    if (info.exists && !info.isDirectory) {
+      setSize(info.size)
+    }
+  })
 }
