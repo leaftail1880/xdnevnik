2bd238d2422c756e417f978cdf309a1c434a4c0c
leaftail1880 <110915645+leaftail1880@users.noreply.github.com>
2025-11-06 14:29:20 +0300

    upd

--- metro.config.js
+++ metro.config.js
@@ -4,6 +4,13 @@ const { getSentryExpoConfig } = require('@sentry/react-native/metro')
 
 const config = getSentryExpoConfig(__dirname)
 
+if (config.resolver) {
+	// @ts-expect-error yes this is expo yes yes i love them so much timeless debugging is my only way to spend time
+	config.resolver.unstable_enablePackageExports = false
+} else {
+	throw new Error('NO RESOLVER IN CONFIG')
+}
+
 module.exports = {
 	...config,
 }
