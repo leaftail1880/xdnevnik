bab6035b5e03e8c56994230e5b1b21c7f5843db6
leaftail1880 <110915645+leaftaul1880@users.noreply.github.com>
2025-11-07 20:39:36 +0300

    fix

--- src/screens/settings/advanced/Advanced.tsx
+++ src/screens/settings/advanced/Advanced.tsx
@@ -15,129 +15,134 @@ import { List, Text } from 'react-native-paper'
 import { DatePickerInput } from 'react-native-paper-dates'
 
 export default observer(function Appearance() {
-	Theme.key
+  Theme.key
 
-	const overrideTime = new Date(XSettings.overrideTimeD)
-	return (
-		<ScrollView>
-			<List.Section title="Общие">
-				<NumberInput
-					label="Timeout"
-					description="Максимальная длительность запроса, в секундах. Чем меньше значение, тем раньше приложение начнет использовать кеш вместо ожидания ответа от сервера"
-					value={API.timeoutLimit}
-					onChange={value => runInAction(() => (API.timeoutLimit = value))}
-					defaultValue={6}
-				/>
-				<NumberInput
-					label="Cache latency"
-					description="Использовать кэш если паралельных запросов больше чем это число. Полезно для оптимизации большого кол-ва запросов. 0 заставляет все запросы возвращать кеш по умолчанию."
-					value={API.useCacheOnMoreThenReqs}
-					onChange={value =>
-						runInAction(() => (API.useCacheOnMoreThenReqs = value))
-					}
-					defaultValue={6}
-				/>
+  const overrideTime = new Date(XSettings.overrideTimeD)
+  return (
+    <ScrollView>
+      <List.Section title="Общие">
+        <NumberInput
+          label="Timeout"
+          description="Максимальная длительность запроса, в секундах. Чем меньше значение, тем раньше приложение начнет использовать кеш вместо ожидания ответа от сервера"
+          value={API.timeoutLimit}
+          onChange={value => runInAction(() => (API.timeoutLimit = value))}
+          defaultValue={6}
+        />
+        <NumberInput
+          label="Cache latency"
+          description="Использовать кэш если паралельных запросов больше чем это число. Полезно для оптимизации большого кол-ва запросов. 0 заставляет все запросы возвращать кеш по умолчанию."
+          value={API.useCacheOnMoreThenReqs}
+          onChange={value =>
+            runInAction(() => (API.useCacheOnMoreThenReqs = value))
+          }
+          defaultValue={6}
+        />
 
-				<View style={{ gap: Spacings.s2 }}>
-					<SwitchSetting
-						setting="useOverrideTime"
-						title="Использовать кастомное время"
-						description="Полезно для демонстрации"
-					/>
-					<SelectTime
-						label="Время в приложении"
-						value={{
-							minutes: overrideTime.getMinutes(),
-							hours: overrideTime.getHours(),
-						}}
-						onSelect={({ hours, minutes }) =>
-							runInAction(() => {
-								overrideTime.setHours(hours, minutes)
-								XSettings.save({ overrideTimeD: overrideTime.getTime() })
-							})
-						}
-						key={overrideTime.getTime().toString()}
-					/>
-					<DatePickerInput
-						locale="ru"
-						value={overrideTime}
-						onChange={d =>
-							!!d && XSettings.save({ overrideTimeD: d.getTime() })
-						}
-						inputMode="start"
-					/>
-				</View>
-			</List.Section>
-			<List.Section title="Хранилище">
-				<SizeOfCache />
-				<List.Item
-					title={
-						<Text>
-							Очистить кэш ({Object.keys(API.cache).length} значений,{' '}
-							<Size t={API.cache} />)
-						</Text>
-					}
-					onPress={() =>
-						runInAction(() => ((API.cache = {}), delete stores['api']))
-					}
-				/>
-				<Stores />
-			</List.Section>
-		</ScrollView>
-	)
+        <View style={{ gap: Spacings.s2 }}>
+          <SwitchSetting
+            setting="useOverrideTime"
+            title="Использовать кастомное время"
+            description="Полезно для демонстрации"
+          />
+          <SelectTime
+            label="Время в приложении"
+            value={{
+              minutes: overrideTime.getMinutes(),
+              hours: overrideTime.getHours(),
+            }}
+            onSelect={({ hours, minutes }) =>
+              runInAction(() => {
+                overrideTime.setHours(hours, minutes)
+                XSettings.save({ overrideTimeD: overrideTime.getTime() })
+              })
+            }
+            key={overrideTime.getTime().toString()}
+          />
+          <DatePickerInput
+            locale="ru"
+            value={overrideTime}
+            onChange={d =>
+              !!d && XSettings.save({ overrideTimeD: d.getTime() })
+            }
+            inputMode="start"
+          />
+        </View>
+      </List.Section>
+      <List.Section title="Хранилище">
+        <ExportImportSettings />
+        <SizeOfCache />
+        <List.Item
+          title={
+            <Text>
+              Очистить кэш ({Object.keys(API.cache).length} значений,{' '}
+              <Size t={API.cache} />)
+            </Text>
+          }
+          onPress={() =>
+            runInAction(() => ((API.cache = {}), delete stores['api']))
+          }
+        />
+        <Stores />
+      </List.Section>
+    </ScrollView>
+  )
 })
 
 const SizeOfCache = observer(function SizeOfCache() {
-	return (
-		<List.Item
-			title={
-				<Text>
-					Занятое место:{' '}
-					<Size
-						t={Object.entries(stores).reduce(
-							(p, e) => p + JSON.stringify(e).length,
-							0,
-						)}
-					/>
-				</Text>
-			}
-		/>
-	)
+  return (
+    <List.Item
+      title={
+        <Text>
+          Занятое место:{' '}
+          <Size
+            t={Object.entries(stores).reduce(
+              (p, e) => p + JSON.stringify(e).length,
+              0,
+            )}
+          />
+        </Text>
+      }
+    />
+  )
+})
+
+const ExportImportSettings = observer(function ExportImportSettings() {
+  return <><List.Item title={<Text>Экспортировать</Text>} /><List.Item title={<Text>Импортировать</Text>} /></>
 })
 
 // eslint-disable-next-line mobx/missing-observer
 function Stores() {
-	return [...PersistStoreMap.values()].map(e => (
-		<Store store={e} key={e.storageName} />
-	))
+  return [...PersistStoreMap.values()].map(e => (
+    <Store store={e} key={e.storageName} />
+  ))
 }
 
 const stores: Record<string, unknown> = observable(
-	{},
-	{},
-	{ defaultDecorator: observable.struct },
+  {},
+  {},
+  { defaultDecorator: observable.struct },
 )
 
 const Store = observer(function Store({
-	store,
+  store,
 }: {
-	store: PersistStore<unknown, never>
+  store: PersistStore<unknown, never>
 }) {
-	useEffect(() => {
-		store.getPersistedStore().then(e => {
-			runInAction(() => {
-				stores[store.storageName] = e
-			})
-		})
-	})
+  useEffect(() => {
+    store.getPersistedStore().then(e => {
+      runInAction(() => {
+        stores[store.storageName] = e
+      })
+    })
+  })
 
-	return (
-		<List.Item
-			title={
-				<Text>
-					{store.storageName}: <Size t={stores[store.storageName]} />
-				</Text>
-			}
-		/>
-	)
+  return (
+    <List.Item
+      title={
+        <Text>
+          {store.storageName}: <Size t={stores[store.storageName]} />
+        </Text>
+      }
+    />
+  )
 })
